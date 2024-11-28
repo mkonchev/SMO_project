f2418d2c42c24ac33bf5311917964c4128338666
Кончев Максим Денисович <konchev.md@edu.spbstu.ru>
2024-11-28 04:30:22 +0300

    Буфер доделан

--- src/buffer_dir/buffer.py
+++ src/buffer_dir/buffer.py
@@ -1,3 +1,5 @@
+import sys
+
 from src.buffer_dir.buffer_item import BufferItem
 from src.order_dir.order import Order
 
@@ -6,6 +8,7 @@ class Buffer:
     """   Буфер   """
     items: list[BufferItem]
     capacity: int
+    mode: bool
 
     def __init__(self, capacity: int) -> None:
         self.capacity = capacity
@@ -17,8 +20,21 @@ class Buffer:
                 return True
         return False
 
-    def add_order(self, order: Order) -> None:
+    def add_order(self, order: Order) -> None:  # Запись в буфер
         for item in self.items:
             if item.is_empty():
                 item.insert_order(order)
 
+    def remove_order(self):  # Выборка из буфера приоритет по номеру источника, по одной заявке
+        order: Order = None
+        min_source_id = sys.int_info.default_max_str_digits
+        deleted_item: BufferItem = None
+        for item in self.items:
+            if not item.is_empty():
+                order_source_id = order.get_source_id()
+                if order_source_id < min_source_id:
+                    min_source_id = order_source_id
+                    order = item.get_order()
+                    deleted_item = item
+        deleted_item.delete_order()
+        return order
