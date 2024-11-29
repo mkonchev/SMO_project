37358236387bac9aa558792eaca69b2c6521735c
mkonchev <maksim1901m@gmail.com>
2024-11-29 04:05:54 +0300

    Источник готов

--- src/source_dir/source.py
+++ src/source_dir/source.py
@@ -1,18 +1,33 @@
+import random as rnd
+
+from src.order_dir.order import Order
+
+
 class Sourse:
     """   Источник заявки   """
     id: int
     order_count: int
-    gen_time: float
+    a: int
+    b: int
+    prev_gen_time: float
 
-    def __init__(self, id: int, order_count: int):
-        self.id = id
-        self.order_count = order_count
+    def __init__(self, id_: int, a_: int, b_: int):
+        self.id = id_
+        self.a = a_
+        self.b = b_
+        self.prev_gen_time = 0.0
 
-    def get_source_id(self):
+    def get_id(self):
         return self.id
 
-    def gen_order(self):
+    def gen_order_count(self):
         self.order_count += 1
 
-    def get_order_count(self):
+    def get_gen_order_count(self):
         return self.order_count
+
+    def order_generate(self):
+        self.prev_gen_time = (self.b - self.a) * rnd.random() + self.a
+        order = Order(self.order_count, self.id, self.prev_gen_time)
+        self.order_count = self.order_count+1
+        return order
---
+++ src/source_dir/source_list.py
@@ -0,0 +1,13 @@
+from source import Sourse
+
+
+class SourceList(object):
+    source_list = list[Sourse]
+
+    def __init__(self, source_count: int, a: int, b: int):
+        self.source_list = [Sourse(i, a, b) for i in range(source_count)]
+
+    def get_source_by_id(self, id_: int) -> Sourse:
+        for source in self.source_list:
+            if source.get_id() == id_:
+                return source
